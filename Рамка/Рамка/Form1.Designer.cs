b2e38d8dd0c75e4f4272ce0e8935fec30eecb676
foskarRU <s.polonski@mail.ru>
2019-12-20 03:31:44 +0500

    то что есть
    все, что есть на данном этапе

---
+++ Рамка/Рамка/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Рамка
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.COMports = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // COMports
+            // 
+            this.COMports.FormattingEnabled = true;
+            this.COMports.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.COMports.Location = new System.Drawing.Point(12, 25);
+            this.COMports.Name = "COMports";
+            this.COMports.Size = new System.Drawing.Size(121, 21);
+            this.COMports.Sorted = true;
+            this.COMports.TabIndex = 2;
+            this.COMports.Text = "Select port";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 52);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(202, 185);
+            this.textBox1.TabIndex = 1;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(139, 25);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 21);
+            this.start.TabIndex = 0;
+            this.start.Text = "Let\'s go";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(228, 255);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.COMports);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox COMports;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button start;
+    }
+}
+
---
+++ Рамка/Рамка/Form1.cs
@@ -0,0 +1,99 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.IO.Ports;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Рамка
+{
+    public partial class Form1 : Form
+    {
+        SerialPort Serial;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] ports = SerialPort.GetPortNames();     //Чтение портов доступных в системе
+            COMports.Items.Clear();                         //Очистка содержимого бокса
+            COMports.Items.AddRange(ports);                 //Добавление найденных портов в бокс    
+            Serial = new SerialPort();
+            Serial.BaudRate = 9600;
+            Serial.DataBits = 8;
+            Serial.StopBits = StopBits.One;
+            Serial.Parity = Parity.None;
+            Serial.DataReceived += new SerialDataReceivedEventHandler(Read);
+
+        }
+
+        ~Form1()
+        {
+            if (Serial.IsOpen) Serial.Close();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            if (!Serial.IsOpen)
+            {
+                Serial.PortName = (string)COMports.SelectedItem;
+                Serial.Open();
+                textBox1.Text += "Подключен к " + Serial.PortName + "\r\n";
+                COMports.Enabled = false;
+                start.Text = "Disconnect";
+            }
+            else
+            {
+                start.Text = "Connect";
+                Serial.Close();
+                textBox1.Text += Serial.PortName + " отчключен" + "\r\n";
+                COMports.Enabled = true;
+            }
+        }
+
+        private void Read(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (!Serial.IsOpen) return;
+            System.Threading.Thread.Sleep(100);
+            string buf = Serial.ReadLine();
+            GetBytes(buf, out string[] bytes);
+            if (textBox1.InvokeRequired)
+            {
+                textBox1.Invoke(new Action(() => textBox1.Text += buf + ": "));
+                textBox1.Invoke(new Action(() => textBox1.ScrollToCaret()));
+
+                string Connect = "Server=127.0.0.1;Port=3306;Database=diplom;User Id=root";
+                MySqlConnection myConnection = new MySqlConnection(Connect);
+
+                string QueryText = "Select Name From Employees WHERE thirdByte=" + bytes[2] + " and fourthByte=" + bytes[3] + "";
+                string getID = "Select ID From Employees WHERE thirdByte=" + bytes[2] + " and fourthByte=" + bytes[3] + "";
+                try
+                {
+                    myConnection.Open();
+                    MySqlCommand myCommand = new MySqlCommand(QueryText, myConnection);
+                    string text = myCommand.ExecuteScalar().ToString() + "\r\n";
+                    MySqlCommand getCommand = new MySqlCommand(getID, myConnection);
+                    string id = getCommand.ExecuteScalar().ToString() + "\r\n";
+                    string insertStatistic = "INSERT INTO `statistic`(`Employee`, `Place`, `Time`) VALUES (" + id[0] + ", " + bytes[4] + ", '" +
+                        DateTime.Now.Year + "-" + DateTime.Now.Month+"-" + DateTime.Now.Day + " " + DateTime.Now.TimeOfDay+ "')";
+                    MySqlCommand insertCommand = new MySqlCommand(insertStatistic, myConnection);
+                    MySqlDataReader reader;
+                    reader = insertCommand.ExecuteReader(); 
+                    textBox1.Invoke(new Action(() => textBox1.Text += text));
+
+                }
+                catch { }
+            }
+            else
+            {
+                textBox1.Text += buf + "\r\n";
+                textBox1.ScrollToCaret();
+            }
+        }
+        
+
+        void GetBytes(string id, out string[] bytes) 
+        {
+           bytes = id.Split(new char[] { ' ' });
+        }
+    }
+}
